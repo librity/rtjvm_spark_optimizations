6a111b4ad73240dd856b2ea4b3003c52ab9e4a7d
Luis <luis.geniole@gmail.com>
2022-11-14 13:33:09 -0300

    Finish Lesson 2.2

---
+++ src/main/scala/section2/SparkJobAnatomy.scala
@@ -0,0 +1,69 @@
+package section2
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+
+import common.{buildJsonPath, inspect, readJsonDF}
+
+object SparkJobAnatomy {
+  /**
+    * Boilerplate
+    */
+  val spark = SparkSession.builder()
+    .appName("Lesson 2.1 - Spark Job Anatomy")
+    .master("local[*]")
+    .getOrCreate()
+
+  import spark.implicits._
+
+  val sc = spark.sparkContext
+  sc.setLogLevel("WARN")
+
+  def main(args: Array[String]): Unit = {
+    /**
+      * Run Spark Shell in the cluster's master node
+      */
+    val rdd1 = sc.parallelize(1 to 1000000)
+
+    rdd1.count()
+    rdd1.map(_ * 2).count()
+    rdd1.repartition(23).count()
+
+
+    /**
+      * Common Aggregation
+      *
+      * - Narrow transformations: isolated on each partition,
+      * like .map(), .mapValues(), .select() and projections
+      * - Wide transformations: All partitions need to be considered,
+      * and there might even be some shuffling, like .groupByKey(), .join() and .orderBy()
+      * - Generally, the less stages the better the performance
+      */
+    // Stage 1
+    val employees = sc.textFile("/tmp/data/employees/employees.csv")
+    val employeeTokens = employees.map(_.split(","))
+    val employeeDetails = employeeTokens.map(tokens => (tokens(4), tokens(7)))
+
+    // Stage 2
+    val employeeGroups = employeeDetails.groupByKey(2)
+    val averageSalaries = employeeGroups.mapValues(salaries => salaries.map(_.toInt).sum / salaries.size)
+    averageSalaries.collect().foreach(println)
+
+
+    /**
+      * Optimized with single map
+      */
+    // Stage 1
+    val employeeDetailsV2 = employees.map { line =>
+      val tokens = line.split(",")
+      (tokens(4), tokens(7))
+    }
+
+    // Stage 2
+    val employeeGroupsV2 = employeeDetailsV2.groupByKey(2)
+    val averageSalariesV2 = employeeGroupsV2.mapValues(salaries => salaries.map(_.toInt).sum / salaries.size)
+    averageSalariesV2.collect().foreach(println)
+
+
+  }
+}
--- src/main/scala/section2/notes.md
+++ src/main/scala/section2/notes.md
@@ -1,1 +1,21 @@
 # Section 2 - Notes
+
+## Run Spark Shell in the cluster's master node
+
+Start Docker Cluster and run bash in the master container:
+
+```bash
+$ docker compose up
+# Copy data to the cluster's master node
+$ docker cp src/main/resources/data spark-cluster-spark-master-1:/tmp
+$ docker exec -it spark-cluster-spark-master-1 bash
+```
+
+Then run the spark shell:
+
+```bash
+$ cd spark/bin
+$ ./spark-shell
+```
+
+Then open [localhost:4040](http://localhost:4040) on your browser for the Web View.
